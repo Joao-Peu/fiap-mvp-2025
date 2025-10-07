458f2bb7e2040705f9d0cf7e4b4e74f54123ec7b
João Pedro <joaoxpedron@gmail.com>
2025-10-07 17:53:12 -0300

    Refatora APIs de usuários para usar UserReadDto
    Adicionada a classe `UserReadDto` para encapsular os dados retornados pelas APIs relacionadas a usuários, promovendo a separação de responsabilidades.
    
    - Alterado o método `GetAll` no `UserController` para mapear os objetos do domínio para `UserReadDto` antes de retorná-los.
    - Alterado o método de busca por ID no `UserController` para retornar um `UserReadDto` em vez do modelo de domínio.
    - Ajustado o construtor do `UserController` para manter a consistência do código.

---
+++ FIAPCloudGames.Application/Dtos/UserReadDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FIAPCloudGames.Application.Dtos
+{
+    public class UserReadDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
